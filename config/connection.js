16d16f78b315320b1f33f6425c5165c8d586376d
Eri <eriinwanderland@Eris-MacBook-Pro.local>
2017-07-29 16:38:52 -0700

    adding burgers to jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,5 @@
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "password",
-//   database: "burgers_db"
-// });
-
 var connection;
 
 if(process.env.JAWSDB_URL){
